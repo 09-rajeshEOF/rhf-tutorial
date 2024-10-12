4c40bb932d77b3c2fd8b3d4f7c9c4ef58dc363bd
Rajesh Pattanaik <rajeshpattanaik56280@gmail.com>
2024-10-12 19:38:34 +0530

    Tailwind CSS Setup

---
+++ src/components/YoutubeForm.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const YoutubeForm = () => {
+    return (
+        <div>
+            <form action="">
+                <label>Username</label>
+                <input type="text" />
+
+                <label>E-Mail</label>
+                <input type="text" />
+
+                <label>Password</label>
+                <input type="text" />
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
